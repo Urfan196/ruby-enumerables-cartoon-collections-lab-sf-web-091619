547df2ff926a8df69a427314d506fd1880076734
Urfan Guliyev <urfan196@yahoo.com>
2019-09-09 16:27:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def long_planeteer_calls (calls)
 end
 
 def find_the_cheese (array)
-  array.include? (cheese_types)
+  array.include? (|item| item == cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
